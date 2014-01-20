6cb9d228ade6baa229fd33f635c40eccd8ed7b5c
saerem21 <nemessisdds>
2014-01-20 11:09:43 +0900

    Initial Commit

---
+++ Ex11_LocalNotiTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ex11_LocalNotiTest
+//
+//  Created by SDT-1 on 2014. 1. 20..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
